00725ca2fa785b6c272415d88e3052e48313e458
stylder <stylder@gmail.com>
2016-12-19 09:07:08 -0600

    Se terminaron los cambios y se acomodó de acuerdo a como dijo Manuelin

--- index.php
+++ index.php
@@ -184,275 +184,13 @@
 <!-- Header END -->
 
 
-<div class="container">
-    <hr>
-    <h1>Polos de Innovación Tecnológica</h1>
-    <p class="text-justify">
-        La intención por impulsar la innovación tecnológica ha generado amplios
-        debates políticos y académicos, sobre todo por la
-        importancia en su desarrollo de la participación de las organizaciones,
-        instituciones e instancias públicas. Es importante
-        analizar el escenario actual iniciando con una reflexión sobre el
-        proceso de innovación en las organizaciones, después,
-        profundizando en aspectos conceptuales de las redes en dirección hacia
-        un conocimiento amplio. El concepto de progreso
-        económico en las economías capitalistas sobre el impacto de las
-        innovaciones tecnológicas se contraponen a la idea del
-        equilibrio general de la economía tal como se describe en la economía
-        neoclásica, (Schumpeter, 1912).
-        El concepto de Polo Tecnológico o Tecnopolo surgio en los 80's y fue
-        idealizada para convertirse en una ciudad del futuro
-        en medio en un campo de girasol en Poitiers, Francia. En San Leopoldo,
-        Campinas, Brasil; fue creado el Polo informático en
-        1999 con la finalidad acoger empresas de base tecnológica y generar un
-        ambiente de sinergia entre dichas empresas,
-        gobierno y el resto de los sectores de la región. Su estructura engloba
-        una incubadora empresarial, un edificio de empresas
-        y un parque tecnológico.
 
-    </p><br>
-    <!-- BEGIN SIDEBAR & CONTENT -->
-    <div class="row">
-        <!-- BEGIN SIDEBAR -->
-        <div class=" col-md-5 col-sm-4">
-            <p class="text-justify">
-                Debido a sus características geográficas de nuestro Estado, el
-                acceso a las tecnologías de información, comunicación y sus
-                servicios relacionados es marginal beneficiando solamente a
-                aquellas comunidades concentradas en poblaciones de alta o
-                mediana densidad; dejando sin acceso a las tecnologías a un alto
-                porcentaje de la población del Estado de Zacatecas.
-                El aprovechamiento de los desarrollos tecnológicos de la
-                actualidad, soluciones y servicios se han consolidado en la
-                plataforma de progreso de cualquier instancia sin excepción
-                incluyendo a cualquier persona, c ontrario a muchos otros
-                proyectos con misión de impulsar la innovación tecnológica, el
-                proyecto de Polos de Innovación Tecnológica en Zacatecas
-                centra sus esfuerzos para intentar llegar a aquellas poblaciones
-                que por diversos factores se encuentran aislados de los
-                proyectos e iniciativas tecnológicas tanto de empresas como de
-                Gobierno en cualquiera de sus niveles.
-            </p>
-        </div>
-        <!-- END SIDEBAR -->
-        <!-- BEGIN CONTENT -->
-        <div class="col-md-7 col-sm-8">
-            <div class="owl-carousel owl-carousel3">
-                <div>
-                    <div class="product-item">
-                        <div class="pi-img-wrapper">
-                            <img src="assets2/img/2.jpg" class="img-responsive"
-                                 alt="Zacatecas">
-                        </div>
-                        <h3><a href="shop-item.html">Zacatecas</a></h3>
-                        <a href="">
-                            <div class="pi-price">COZCYT</div>
-                        </a>
-                        <a href="#" class="btn btn-default add2cart">LABSOL</a>
-                    </div>
-                </div>
-                <div>
-                    <div class="product-item">
-                        <div class="pi-img-wrapper">
-                            <img src="assets2/img/4.jpg" class="img-responsive"
-                                 alt="Zacatecas">
-                        </div>
-                        <h3><a href="shop-item.html">Zacatecas</a></h3>
-                        <a href="">
-                            <div class="pi-price">COZCYT</div>
-                        </a>
-                        <a href="#" class="btn btn-default add2cart">LABSOL</a>
-                    </div>
-                </div>
-                <div>
-                    <div class="product-item">
-                        <div class="pi-img-wrapper">
-                            <img src="assets2/img/5.jpg" class="img-responsive"
-                                 alt="Zacatecas">
-                        </div>
-                        <h3><a href="shop-item.html">Zacatecas</a></h3>
-                        <a href="">
-                            <div class="pi-price">COZCYT</div>
-                        </a>
-                        <a href="#" class="btn btn-default add2cart">LABSOL</a>
-                    </div>
-                </div>
-                <div>
-                    <div class="product-item">
-                        <div class="pi-img-wrapper">
-                            <img src="assets2/img/7.jpg" class="img-responsive"
-                                 alt="Zacatecas">
-                        </div>
-                        <h3><a href="shop-item.html">Zacatecas</a></h3>
-                        <a href="">
-                            <div class="pi-price">COZCYT</div>
-                        </a>
-                        <a href="#" class="btn btn-default add2cart">LABSOL</a>
-                    </div>
-                </div>
-                <div>
-                    <div class="product-item">
-                        <div class="pi-img-wrapper">
-                            <img src="assets2/img/10.jpg" class="img-responsive"
-                                 alt="Zacatecas">
-                        </div>
-                        <h3><a href="shop-item.html">Zacatecas</a></h3>
-                        <a href="">
-                            <div class="pi-price">COZCYT</div>
-                        </a>
-                        <a href="#" class="btn btn-default add2cart">LABSOL</a>
-                    </div>
-                </div>
-                <div>
-                    <div class="product-item">
-                        <div class="pi-img-wrapper">
-                            <img src="assets2/img/8.jpg" class="img-responsive"
-                                 alt="Zacatecas">
-                        </div>
-                        <h3><a href="shop-item.html">Zacatecas</a></h3>
-                        <a href="">
-                            <div class="pi-price">COZCYT</div>
-                        </a>
-                        <a href="#" class="btn btn-default add2cart">LABSOL</a>
-                    </div>
-                </div>
-                <div>
-                    <div class="product-item">
-                        <div class="pi-img-wrapper">
-                            <img src="assets2/img/9.jpg" class="img-responsive"
-                                 alt="Zacatecas">
-                        </div>
-                        <h3><a href="shop-item.html">Zacatecas</a></h3>
-                        <a href="">
-                            <div class="pi-price">COZCYT</div>
-                        </a>
-                        <a href="#" class="btn btn-default add2cart">LABSOL</a>
-                    </div>
-                </div>
-
-            </div>
-        </div>
-        <!-- END CONTENT -->
-    </div>
-    <!-- END SIDEBAR & CONTENT -->
-    <p class="text-justify">
-        Los Polos de Innovación Tecnológica se diseña de tal forma que
-        comprometa a participar a instancias de gobierno estatal y
-        municipales, instituciones de educación y empresas de base tecnológica
-        de cada región, focalizando estos esfuerzos para
-        beneficio de los sectores mencionados pero de manera prioritaria para el
-        beneficio social de las regiones.
-        El Laboratorio de Software Libre (LABSOL) del Consejo Zacatecano de
-        Ciencia, Tecnología e Innovación (COZCyT) adopto
-        como una estrategia principal la vinculación y colaboración con
-        instituciones de educación media superior y superior de
-        todo el Estado, con esta vinculación se ha convertido en un importante
-        centro de desarrollo tecnológico con un catálogo
-        de proyectos desarrollados que supera los 60 y cada semestre se
-        desarrollan cuando menos una docena más; al ser
-        proyectos basados en software libre y de código abierto pueden ser
-        aprovechados por cualquiera para su implantación; la
-        intención es que los Polos de Innovación Tecnológica colaboren
-        estrechamente con el LABSOL para que dichos proyectos
-        puedan ser utilizados por los Polos e impulsar su misión.
-        <br><br>
-    </p>
-    <hr>
-</div>
 <br><br>
-<div class="main">
-    <div class="container">
-        <h1>Generación y difusión del conocimiento</h1>
-        <!-- BEGIN TWO PRODUCTS & PROMO -->
-        <div class="row margin-bottom-35 ">
-            <!-- BEGIN TWO PRODUCTS -->
-            <div class="col-md-3">
-
-
-            </div>
-            <!-- END TWO PRODUCTS -->
-            <!-- BEGIN PROMO -->
-            <div class="col-md-9 shop-index-carousel">
-                <div class="content-slider">
-                    <div id="myCarousel" class="carousel slide"
-                         data-ride="carousel">
-                        <!-- Indicators -->
-                        <ol class="carousel-indicators">
-                            <li data-target="#myCarousel" data-slide-to="0"
-                                class="active"></li>
-                            <li data-target="#myCarousel"
-                                data-slide-to="1"></li>
-                            <li data-target="#myCarousel"
-                                data-slide-to="2"></li>
-                            <li data-target="#myCarousel"
-                                data-slide-to="3"></li>
-                            <li data-target="#myCarousel"
-                                data-slide-to="4"></li>
-                        </ol>
-                        <div style="margin-top: 30px;" class="carousel-inner">
-                            <div class="item active">
-                                <img style="height: 280px;"
-                                     src="assets2/img/home/1.jpg"
-                                     class="img-responsive">
-                            </div>
-                            <div class="item">
-                                <img style="height: 280px;"
-                                     src="assets2/img/home/2.jpg"
-                                     class="img-responsive">
-                            </div>
-                            <div class="item">
-                                <img style="height: 280px;"
-                                     src="assets2/img/home/3.jpg"
-                                     class="img-responsive">
-                            </div>
-
-                            <div class="item">
-                                <img style="height: 280px;"
-                                     src="assets2/img/home/lab2.jpg"
-                                     class="img-responsive">
-                            </div>
-
-
-                            <div class="item">
-                                <img style="height: 280px;"
-                                     src="assets2/img/home/lab.jpg"
-                                     class="img-responsive">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- END PROMO -->
-        </div>
-        <!-- END TWO PRODUCTS & PROMO -->
-    </div>
-</div>
 
 <div class="container">
-    <br>
-    <p class="text-justify">
-        Actualmente Zacatecas se consolida con un liderazgo y escanerio único en
-        nuestro País e incluso en otros paises en el tema
-        de innovación tecnológica abierta basada en los fundamentos del software
-        libre y de código abierto, la Ley Estatal de
-        Software Libre obliga a instancias públicas a la adopción de software
-        libre en proyectos tecnológicos; al Laboratorio de
-        Software Libre del COZCyT se ha consolidado como un centro especializado
-        en el desarrollo de proyectos tecnológicos y lo
-        anterior fortalecido por el Cluster de Software Libre que enfoca su
-        misión en el impulso del sector empresarial del Estado
-        hacia la innovación en negocios aprovechando las soluciones de software
-        libre y código abierto.
-    </p>
-</div>
 
-
-<div class="container">
-    <hr>
     <h1>Talleres</h1>
-    <div class="col-md-6">
-        <a class="twitter-timeline" href="https://twitter.com/polosinnovzac">Tweets by polosinnovzac</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
-    </div>
+
     <div class="col-sm-6">
         <div class="col-xs-12" id="slider">
             <!-- Top part of the slider -->
@@ -491,6 +229,10 @@
             </div>
         </div>
     </div>
+    <div class="col-md-6">
+        <a class="twitter-timeline" data-lang="es" data-width="550" href="https://twitter.com/polosinnovzac">Tweets by polosinnovzac</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </div>
+
 
 </div>
 
@@ -586,6 +328,7 @@
 </div>
 
 
+
 <div class="container">
     <hr>
     <h1>Ubicación</h1>
@@ -637,6 +380,96 @@
 </div>
 
 
+
+
+
+<div class="main">
+
+    <div class="container">
+        <hr>
+        <h1>Generación y difusión del conocimiento</h1>
+        <!-- BEGIN TWO PRODUCTS & PROMO -->
+        <div class="row margin-bottom-35 ">
+            <!-- BEGIN TWO PRODUCTS -->
+            <div class="col-md-3">
+
+
+            </div>
+            <!-- END TWO PRODUCTS -->
+            <!-- BEGIN PROMO -->
+            <div class="col-md-9 shop-index-carousel">
+                <div class="content-slider">
+                    <div id="myCarousel" class="carousel slide"
+                         data-ride="carousel">
+                        <!-- Indicators -->
+                        <ol class="carousel-indicators">
+                            <li data-target="#myCarousel" data-slide-to="0"
+                                class="active"></li>
+                            <li data-target="#myCarousel"
+                                data-slide-to="1"></li>
+                            <li data-target="#myCarousel"
+                                data-slide-to="2"></li>
+                            <li data-target="#myCarousel"
+                                data-slide-to="3"></li>
+                            <li data-target="#myCarousel"
+                                data-slide-to="4"></li>
+                        </ol>
+                        <div style="margin-top: 30px;" class="carousel-inner">
+                            <div class="item active">
+                                <img style="height: 280px;"
+                                     src="assets2/img/home/1.jpg"
+                                     class="img-responsive">
+                            </div>
+                            <div class="item">
+                                <img style="height: 280px;"
+                                     src="assets2/img/home/2.jpg"
+                                     class="img-responsive">
+                            </div>
+                            <div class="item">
+                                <img style="height: 280px;"
+                                     src="assets2/img/home/3.jpg"
+                                     class="img-responsive">
+                            </div>
+
+                            <div class="item">
+                                <img style="height: 280px;"
+                                     src="assets2/img/home/lab2.jpg"
+                                     class="img-responsive">
+                            </div>
+
+
+                            <div class="item">
+                                <img style="height: 280px;"
+                                     src="assets2/img/home/lab.jpg"
+                                     class="img-responsive">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- END PROMO -->
+        </div>
+        <!-- END TWO PRODUCTS & PROMO -->
+    </div>
+</div>
+<div class="container">
+    <br>
+    <p class="text-justify">
+        Actualmente Zacatecas se consolida con un liderazgo y escanerio único en
+        nuestro País e incluso en otros paises en el tema
+        de innovación tecnológica abierta basada en los fundamentos del software
+        libre y de código abierto, la Ley Estatal de
+        Software Libre obliga a instancias públicas a la adopción de software
+        libre en proyectos tecnológicos; al Laboratorio de
+        Software Libre del COZCyT se ha consolidado como un centro especializado
+        en el desarrollo de proyectos tecnológicos y lo
+        anterior fortalecido por el Cluster de Software Libre que enfoca su
+        misión en el impulso del sector empresarial del Estado
+        hacia la innovación en negocios aprovechando las soluciones de software
+        libre y código abierto.
+    </p>
+</div>
+
 <div class="container">
     <div class="my-logo-slider">
 
@@ -668,6 +501,10 @@
         </div>
     </div>
 </div>
+
+
+
+
 
 
 
